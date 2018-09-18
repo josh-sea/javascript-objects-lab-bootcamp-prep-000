b22e210a67f74994866dddf8a4c1a3d225976d21
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-09-18 00:45:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-   var obj = Object.assign({}, object, { [key]: value })
-   return object, obj
+   var obj = Object.assign({}, object, { [key]: value });
+   return object, obj;
+ }
+ 
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+   
  }
